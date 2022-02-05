e7443375b21767419b846571c467c512b360b5b9
Leonardo Massafelli <lmassafelli@yahoo.com.br>
2022-02-05 17:10:49 -0300

    uploading files

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // Add ;
+  trailingComma: 'all', // add a pending , at obj
+  singleQuote: true, // use ''
+  printWidth: 80, // line breaking %
+  tabWidth: 2, // tab width
+};
